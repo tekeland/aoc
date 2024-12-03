d070c0cbbc35f480425d0cc8438c3ba196f9ca96
Andrej Tekel <andrej.tekel@erstegroup.com>
2024-12-03 11:24:00 +0100

    Add solution for Day01

---
+++ src/day01/Day01.kt
@@ -0,0 +1,45 @@
+package day01
+
+import println
+import readInput
+import kotlin.math.abs
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val (left, right) = parseInput(input)
+        val distances = left.zip(right).map { abs(it.first - it.second) }
+
+        return distances.sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        val (left, right) = parseInput(input)
+        var score = 0
+        left.forEach { leftNumber -> score += right.count(leftNumber::equals) * leftNumber }
+
+        return score
+    }
+
+    val testInput = readInput("Day01_test")
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
+
+    val input = readInput("Day01")
+    part1(input).println()
+    part2(input).println()
+}
+
+private fun parseInput(input: List<String>): Pair<List<Int>, List<Int>> {
+    val left = mutableListOf<Int>()
+    val right = mutableListOf<Int>()
+    input.map { line ->
+        val (leftPart, rightPart) = line.split("   ")
+        left.add(leftPart.toInt())
+        right.add(rightPart.toInt())
+    }
+
+    left.sort()
+    right.sort()
+
+    return left to right
+}
